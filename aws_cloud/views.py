20739704db85b97ec0b5eb4dd6d4d43360470f76
Ashish Kathuria <xs2ashish@gmail.com>
2019-11-19 23:58:14 +0000

    2019-11-19

--- aws_cloud/views.py
+++ aws_cloud/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+services=[
+    {
+        'service_name' : 'EC2',
+        'service_type' : 'Compute',
+        'AVailable_resources' : 'Linux,Windows' 
+    },
+        {
+        'service_name' : 'RDS',
+        'service_type' : 'Database',
+        'Available_resources' : 'PostgreSQL,MariaDB,MySQL' 
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>AWS Cloud</h1>')
+    context={
+        'srvcs' : services
+    }
+    return render(request,'aws_cloud/aws_home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>AWS Cloud About page</h1>')
+    return render(request,'aws_cloud/aws_about.html')
--- cloud_manager/urls.py
+++ cloud_manager/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('aws_cloud.urls')),
+    path('awscloud/', include('aws_cloud.urls')),
 ]
