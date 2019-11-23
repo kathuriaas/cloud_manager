4b5a463de0135d9d5559b502700b6643ac70fced
Ubuntu <ubuntu@ip-172-31-26-223.us-east-2.compute.internal>
2019-11-23 12:38:26 +0000

    2019-11-23

--- cloud_manager/urls.py
+++ cloud_manager/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('clouds/', include('clouds.urls')),
+    path('', include('clouds.urls')),
 ]
