77e548adacd51dc2ffe09f07e9a30ff0619bab6c
Ubuntu <ubuntu@ip-172-31-26-223.us-east-2.compute.internal>
2019-11-23 12:28:14 +0000

    2019-11-23

--- clouds/views.py
+++ clouds/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+services=[
+    {
+        'service_name' : 'EC2',
+        'service_type' : 'Compute',
+        'AVailable_resources' : 'Linux,Windows' 
+    },
+        {
+        'service_name' : 'RDS',
+        'service_type' : 'Database',
+        'Available_resources' : 'PostgreSQL,MariaDB,MySQL' 
+    }
+]
+
+def home(request):
+    context={
+        'srvcs' : services
+    }
+    return render(request,'aws_cloud/aws_home.html',context)
+
+def about(request):
+    return render(request,'aws_cloud/aws_about.html')
