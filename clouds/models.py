88ac25177331fe5eb496c14cd4dd8528d8beca65
Ashish Kathuria <xs2ashish@gmail.com>
2019-11-27 14:54:31 +0000

    2019-11-27

--- clouds/models.py
+++ clouds/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Servives(models.Model):
+    service_name = models.CharField(max_length=30)
+    service_type = models.CharField(max_length=30)
+    services_supported = models.TextField()
